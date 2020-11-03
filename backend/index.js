c3218014eec724f4359b895a949bac4b96817e09
EstebanRuiz23 <esteban.alvarez.286@gmail.com>
2020-11-03 14:18:56 -0500

    se corrigen las rutas y se agrega el inicio y el cerrado de sesion

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,11 @@ const usuarios_clases = require('./routes/usuario_clases');
 const usuario_cursos = require('./routes/usuario_curso');
 app.set('port', 3883);
 app.use(express.json());
-app.use('/usuarios', usuarios);
-app.use('/cursos', cursos);
-app.use('/clases', clases);
-app.use('/usuario_clases', usuarios_clases);
-app.use('/usuario_curso', usuario_cursos);
+app.use('/', usuarios);
+app.use('/', cursos);
+app.use('/', clases);
+app.use('/', usuarios_clases);
+app.use('/', usuario_cursos);
 app.listen(app.get('port'), () => {
     console.log('server on port' + app.get('port'));
 });
--- backend/routes/usuarios.js
+++ backend/routes/usuarios.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const router = Router();
 const mysqlconection = require('../db/db');
-
+/* inicio de sesion */
 router.get('/:usuario', (req, res) => {
     const { usuario } = req.params;
     mysqlconection.query('SELECT * FROM usuarios WHERE usuario = ?', [usuario], (error, rows, fields) => {
@@ -12,7 +12,7 @@ router.get('/:usuario', (req, res) => {
         }
     });
 });
-
+/* registrar usuario */
 router.post('/', (req, res) => {
     const { nombre, apellido, genero, fecha_n, edad, usuario, contraseña, correo } = req.body;
     let usuarioA = [nombre, apellido, genero, fecha_n, edad, usuario, contraseña, correo];
@@ -22,6 +22,30 @@ router.post('/', (req, res) => {
             console.error(err);
         } else {
             res.json({ message: 'Usuario fue registrado.' });
+        }
+    });
+});
+/* cerrar sesion */
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    let querycerrarsesion = 'UPTADE usuarios SET estado=false WHERE id=?';
+    mysqlconection.query(querycerrarsesion, [id], (err, results, fields) => {
+        if (err) {
+            console.error(err);
+        } else {
+            res.json({ message: 'se cerro sesion correctamente.' });
+        }
+    });
+});
+/* iniciar sesion */
+router.put('/principal/:id', (req, res) => {
+    const { id } = req.params;
+    let queryiniciarsesion = 'UPTADE usuarios SET estado=true WHERE id=?';
+    mysqlconection.query(queryiniciarsesion, [id], (err, results, fields) => {
+        if (err) {
+            console.error(err);
+        } else {
+            res.json({ message: 'se cerro sesion correctamente.' });
         }
     });
 });
