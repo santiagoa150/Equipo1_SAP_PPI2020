af07d299be9cb80ed9fda91516e5d06497c5bd60
jucamo0713 <jucamo.0713@gmail.com>
2020-11-29 05:01:50 -0500

    cosas

--- frontend/src/Components/Main4.jsx
+++ frontend/src/Components/Main4.jsx
@@ -12,7 +12,8 @@ class Main4 extends React.Component {
             modal1: {
                 modal1bool: false
             },
-            boool: false
+            boool: false,
+            modal2bool: false
 
         }
     }
@@ -58,6 +59,40 @@ class Main4 extends React.Component {
 
         }
     }
+
+    modal2Return = () => {
+        if (this.state.modal2bool) {
+            return (
+                <>
+                    <div className="ModalFondo">
+                        <div className="Modal1">
+                            <div className="ModalTitle">
+                                <h2>Incrustado</h2>
+                            </div>
+
+                            <p>
+                                {() => {
+                                    if (this.state.CursoData[0].d_permiso==1) {
+                                        return "El codigo de incrustado de este juego es: "+this.state.CursoData[0].id ;
+                                    }else{
+                                        return "El creador de este curso lo bloqueo para que no se pudiera usar su juego como incrustado";
+                                    }
+                                }}
+                            </p>
+
+                            <button className="button buttonModal1" onClick={() => {
+                                this.setState({
+                                    modal2bool: !this.state.modal2bool
+                                })
+                            }
+                            }>Cancelar</button>
+                        </div>
+                    </div>
+                </>
+            );
+
+        }
+    }
     getInfoCurso = async () => {
         await axios.get(`http://localhost:3883/Cur/get_cursos-Comunidad_Integrado/Curso/${this.props.location.state.id}`)
             .then(res => {
@@ -84,12 +119,14 @@ class Main4 extends React.Component {
         return (
             <>
                 {this.modal1Return()}
+                {this.modal2Return()}
                 <div className="Cargando" id="carga"></div>
                 <div id="Main4Container">
                     <div id="TeoriaCurso">
                     </div>
-                    <div>
-                        <Link id="DidaCurso" to={{
+                    <div id="DidaCurso">
+                        <i></i>
+                        <Link className="LinkCursoMain4" to={{
                             pathname: "/Didactico",
                             state: {
                                 id: this.state.CursoData[0]?.id,
@@ -98,8 +135,14 @@ class Main4 extends React.Component {
                             }
                         }}>
 
-                            <button className="LinkCursoMain4">Jugar</button>
+                            <button >Jugar</button>
                         </Link>
+                        <img src="./images/InfoCurso.png" className="incrustado" onClick={() => {
+                                this.setState({
+                                    modal2bool: !this.state.modal2bool
+                                })
+                            }
+                            }/>
                     </div>
                     <div id="ExaCurso">
                         <button className="LinkCursoMain4" onClick={() => {
